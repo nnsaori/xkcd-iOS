7bd8428aab44d856f45d7c5b096c802c9b7b3779
Saori <saori07208@gmail.com>
2021-11-23 08:21:06 +0900

    Initial commit

---
+++ xkcd-iOS/xkcd-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xkcd-iOS
+//
+//  Created by Saori Kurimoto on 2021/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
